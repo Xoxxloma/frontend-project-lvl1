793c81a1880eedbf6a58de8ca7eb23608c0f381f
xoxxloma <xoxxloma@mail.ru>
2019-12-20 13:08:38 +0300

    fixes

--- src/games/even.js
+++ src/games/even.js
@@ -8,7 +8,7 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 const isEven = (value) => value % 2 === 0;
 
 const getQuestionAnswer = () => {
-  const question = getRandom(1, 99)();
+  const question = getRandom(1, 99);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return cons(question, correctAnswer);
 };
