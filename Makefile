66419bf4174c1a19303c8e4a623932bd83f549e0
xoxxloma <xoxxloma@mail.ru>
2019-12-20 19:11:18 +0300

    added last game

--- Makefile
+++ Makefile
@@ -21,3 +21,6 @@ brain-gcd:
 
 brain-progression:
 	npx babel-node src/bin/brain-progression.js
+
+brain-prime:
+	npx babel-node src/bin/brain-prime.js
---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { cons } from '@hexlet/pairs';
+import getRandom from '../helpers';
+import gameEngine from '..';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num < 1) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getQuestionAnswer = () => {
+  const num = getRandom(1, 101);
+  const question = `${num}`;
+  const correctAnswer = isPrime(num) ? 'yes' : 'no';
+  return cons(question, correctAnswer);
+};
+
+export default () => gameEngine(description, getQuestionAnswer);
